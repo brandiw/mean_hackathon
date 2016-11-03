6ffdb104afb49a7c82220aa077588556bb2ca6f7
sarahpappas <pappas.sarah@gmail.com>
2016-11-03 11:07:28 -0700

    updating from airplanes framework

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var bodyParser = require('body-parser');
 var app = express();
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/airplanes');
+mongoose.connect('mongodb://localhost/eightball');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require('morgan')('dev'));
 
-app.use('/api/airplanes', require('./controllers/airplane'));
+app.use('/api/verdict', require('./controllers/verdict'));
 
 app.get('/*', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
