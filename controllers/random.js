5f29c0140428b9dd3cd340a52b6d6d0298992fe9
jedimasterclos <carlossrinivas@gmail.com>
2016-11-03 13:23:14 -0700

    Carlos: completed idea generator

--- controllers/random.js
+++ controllers/random.js
@@ -4,18 +4,46 @@ var Wordnik = require('wordnik-bb').init(APIKEY);
 var express = require('express');
 var router = express.Router();
 
+var noun = "";
+var verb = "";
+var nouns = "";
+
+
 router.route('/')
 .get(function(req, res) {
   var randomWordPromise = Wordnik.getRandomWordModel({
+      hasDictionaryDef: false,
+      includePartOfSpeech: "adjective",
+      minCorpusCount: 10000
+  });
+  var randomWordNoun = Wordnik.getRandomWordModel({
+      hasDictionaryDef: false,
+      includePartOfSpeech: "noun",
+      minCorpusCount: 10000
+  });
+  randomWordNoun.done(function(word) {
+    noun = word.attributes.word
+  });
+  var randomWordVerb = Wordnik.getRandomWordModel({
+      hasDictionaryDef: false,
       includePartOfSpeech: "verb",
       minCorpusCount: 10000
-    }
-  );
+  });
+  randomWordVerb.done(function(word) {
+    verb = word.attributes.word
+  });
+  var randomWordNouns = Wordnik.getRandomWordModel({
+      hasDictionaryDef: false,
+      includePartOfSpeech: "noun",
+      minCorpusCount: 10000
+  });
+  randomWordNouns.done(function(word) {
+    nouns = word.attributes.word
+  });
   randomWordPromise.done(function(wordModel) {
-    res.send(wordModel.attributes.word);
+    res.send("The " + wordModel.attributes.word + " " + noun + " that " + verb + " " + nouns);
   });
 });
 
-
-
 module.exports = router;
+
