2ddb90cf71c1eb8fffce2ac81c118fc2864fb98a
britbroet <brittanybroetje@gmail.com>
2016-11-03 15:50:52 -0700

    shake button connected

--- public/app/components/eightballMessage/eightballMessage.js
+++ public/app/components/eightballMessage/eightballMessage.js
@@ -7,21 +7,25 @@
   });
 
   function EightballMessage(EightballService) {
-    console.log('firing eightball controller');
     var eightballmsg = this;
     eightballmsg.eightballs = [];
 
     EightballService.getAllEightballs(function(data) {
-      console.log('firing get all eightballs HERE data.data', data.data[1].content);
-      console.log('firing get all eightballs HERE data', data);
       eightballmsg.eightballs = data.data;
 
       var randomIndex = Math.floor((Math.random() * 5));
       eightballmsg.randommsg = data.data[randomIndex].content;
+
     });
 
     console.log('HUZZAH!');
+
+    eightballmsg.shakeIt = function() {
+        console.log('ITS SHAKING');
+      }
+
   }
+
 
   EightballMessage.$inject = ['EightballService'];
 })()
